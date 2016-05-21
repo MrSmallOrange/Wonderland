73456fd46dfc9f5979baf826106ad3cce5af2be8
zhangsan <13413412>
2016-05-21 22:46:17 +0800

    初始化

---
+++ Wonderland/Wonderland/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Wonderland
+//
+//  Created by ZhangCheng on 16/5/21.
+//  Copyright © 2016年 ZhangCheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Wonderland/WonderlandTests/WonderlandTests.m
@@ -0,0 +1,39 @@
+//
+//  WonderlandTests.m
+//  WonderlandTests
+//
+//  Created by ZhangCheng on 16/5/21.
+//  Copyright © 2016年 ZhangCheng. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WonderlandTests : XCTestCase
+
+@end
+
+@implementation WonderlandTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
